6646433af1346c903350da805728fe30315f9b67
erokemwa <111133254+erokemwa@users.noreply.github.com>
2024-01-15 07:51:12 +0300

    Code Log

--- codelog.py
+++ codelog.py
@@ -1,6 +1,32 @@
-from src import create_app
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
-app = create_app()
+posts = [
+    {
+        'author': 'Eric Okemwa',
+        'title': 'Code Log',
+        'content': 'First post content',
+        'date_posted': 'January 15, 2018'
+    },
+    {
+        'author': 'Peter White',
+        'title': 'Code Log 2',
+        'content': 'Second post content',
+        'date_posted': 'January 15, 2023'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
